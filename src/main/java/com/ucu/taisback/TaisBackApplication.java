5cbfb38da6a38ecc82457409ed15059529957c36
Bettina Carrizo <betticarrizo@gmail.com>
2022-10-30 11:21:26 -0300

    FIX: Agregar https al Origin

--- src/main/java/com/ucu/taisback/TaisBackApplication.java
+++ src/main/java/com/ucu/taisback/TaisBackApplication.java
@@ -40,7 +40,7 @@ public class TaisBackApplication {
 	public CorsFilter corsFilter(){
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("qrlink-dev.web.app/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://qrlink-dev.web.app/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type",
 				"Accept","Authorization","Origin, Accept", "X-Requested-With","Access-Control-Request-Method",
 				"Access-Control-Request-Headers"));
