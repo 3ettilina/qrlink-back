0d14b7b2b36e27a464bd11339182f8c303415d8b
Bettina Carrizo <betticarrizo@gmail.com>
2022-10-30 11:04:54 -0300

    FIX: Unable to start Tomcat - Actualizar CORS en App.java

--- src/main/java/com/ucu/taisback/TaisBackApplication.java
+++ src/main/java/com/ucu/taisback/TaisBackApplication.java
@@ -40,9 +40,9 @@ public class TaisBackApplication {
 	public CorsFilter corsFilter(){
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://qrlink-dev.web.app/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("qrlink-dev.web.app/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type",
-				"Accept","Authorization","Origin", "Accept", "X-Requested-With","Access-Control-Request-Method",
+				"Accept","Authorization","Origin, Accept", "X-Requested-With","Access-Control-Request-Method",
 				"Access-Control-Request-Headers"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin","Content-Type","Accept","Authorization",
 				"Access-Control-Allow-Origin","Access-Control-Allow-Credentials"));
