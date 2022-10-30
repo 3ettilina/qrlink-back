64d9364184ee53f07812fa0801b4fe2d264237ab
Bettina Carrizo <betticarrizo@gmail.com>
2022-10-30 10:50:45 -0300

    FIX: Unable to start Tomcat - Update corsFilter con código viejo

--- src/main/java/com/ucu/taisback/TaisBackApplication.java
+++ src/main/java/com/ucu/taisback/TaisBackApplication.java
@@ -42,14 +42,14 @@ public class TaisBackApplication {
 		corsConfiguration.setAllowCredentials(true);
 		corsConfiguration.setAllowedOrigins(Arrays.asList("https://qrlink-dev.web.app/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type",
-				"Accept","Authorization","Origin, Accept", "X-Requested-With","Access-Control-Request-Method",
+				"Accept","Authorization","Origin", "Accept", "X-Requested-With","Access-Control-Request-Method",
 				"Access-Control-Request-Headers"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin","Content-Type","Accept","Authorization",
 				"Access-Control-Allow-Origin","Access-Control-Allow-Credentials"));
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);
-		return new CorsFilter((CorsConfigurationSource) urlBasedCorsConfigurationSource);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
 
 }
