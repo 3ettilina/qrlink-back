6f64fb68df2074c509cfbd2351e05b7d27e8b40e
Franco Gai <francogai.globant@latam.com>
2022-11-13 17:48:07 -0300

    agregado endpoint para agregar producto y modificadas las entidades acorde a lo mostrado en la task

--- src/main/java/com/ucu/taisback/service/implementation/AddProductImplementation.java
+++ src/main/java/com/ucu/taisback/service/implementation/AddProductImplementation.java
@@ -7,16 +7,20 @@ import java.util.ArrayList;
 
 public class AddProductImplementation {
 
+  public static final String DEFAULT_LINK = "gs1:defaultLink";
+
   public static Product buildProduct(Product product){
     Resource resource = new Resource();
     resource.setName(product.getName());
     resource.setResource_url(product.getResource_url());
-    resource.setLink_type(product.getLink_type());
+    resource.setLink_type(DEFAULT_LINK);
 
     ArrayList<Resource> resources = new ArrayList<>();
     resources.add(resource);
 
     product.setResources(resources);
+    product.setResource_url(null);
+    product.setName(null);
 
     return product;
 
