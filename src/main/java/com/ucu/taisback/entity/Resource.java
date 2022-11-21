d485fe2f72ba11a223fcf1e7b96a96aac7a2fc78
Franco Gai <francogai.globant@latam.com>
2022-11-21 11:59:29 -0300

    bug 107: solved bugfix

--- src/main/java/com/ucu/taisback/entity/Resource.java
+++ src/main/java/com/ucu/taisback/entity/Resource.java
@@ -1,14 +1,17 @@
 package com.ucu.taisback.entity;
 
+import com.google.firebase.database.annotations.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Resource {
+  @NotNull
   private String name;
   private String link_type;
   private String language;
+  @NotNull
   private String resource_url;
 }
 
